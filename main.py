598c13ff6c0112eb02513c8d149779811b91abf5
TierGamerpy <adityabhushan934@gmail.com>
2021-04-15 21:00:48 +0530

    all done
    1. done paginitation,
    2. done logout,
    3. fixed add new post,
    4. add file uploader.
    All Set You Can Use This Template Now

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import os
 from flask import Flask, render_template, request,redirect
 from flask import session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
+import math
+
+from werkzeug.utils import secure_filename
 
 with open('config.json','r') as c:
     params = json.load(c)['params']
 local_server = True
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
+app.config['UPLOAD_FOLDER'] = params['upload_location']
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Aditya@9457@localhost/testdb" 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -32,8 +37,31 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:5]
-    return render_template('index.html',params=params,posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts) / int(params['no_of_posts_to_display_on_home_at_once']))
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page=int(page)
+
+    posts = posts[(page-1)*int(params['no_of_posts_to_display_on_home_at_once']) : (page-1)*int(params['no_of_posts_to_display_on_home_at_once']) + int(params['no_of_posts_to_display_on_home_at_once'])]
+    if page ==1:
+        prev_backgroun = 'disabled'
+        next_backgroun = ''
+        prev = "#"
+        next = "/?page="+str(page+1)
+    elif page==last:
+        prev_backgroun = ''
+        next_backgroun = 'disabled'
+        prev = "/?page="+str(page-1)
+        next = "#"
+    else:
+        prev_backgroun = ''
+        next_backgroun = ''
+        prev = "/?page="+str(page-1)
+        next = "/?page="+str(page+1)
+
+    return render_template('index.html',params=params,posts=posts,prev = prev,next = next,prev_background=prev_backgroun,next_background=next_backgroun)
 
 
 @app.route("/about")
@@ -97,6 +125,7 @@ def edit(sno):
                 post = Posts(title=title, slug=slug, content=content, tagline=tline, img_file=img_file, date=date)
                 db.session.add(post)
                 db.session.commit()
+                return redirect('/dashboard')
             else:
                 post = Posts.query.filter_by(sno=sno).first()
                 post.title = title
@@ -108,7 +137,32 @@ def edit(sno):
                 db.session.commit()
                 return redirect('/dashboard')
         post = Posts.query.filter_by(sno=sno).first()
-        return render_template('edit.html',params=params,post = post)
+        return render_template('edit.html',params=params,post = post,sno=sno)
     else:
         return render_template("login.html", params=params)
+
+@app.route("/uploader", methods = ['GET', 'POST'])
+def uploader():
+    if "user" in session and session["user"] == params["admin_user"]:
+        if request.method == 'POST':
+            f = request.files['file1']
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+            return 'Uploaded SuccessFully'
+    else:
+        return render_template("login.html", params=params)
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect('/')
+
+@app.route("/delete/<string:sno>")
+def delete(sno):
+    if "user" in session and session["user"] == params["admin_user"]:
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+        return redirect('/dashboard')
+    else:
+        return render_template("login.html", params=params)   
 app.run(debug=True)
